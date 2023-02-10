fe7de7b3a018eb0c67c41276cc7a3f8932ef078b
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-02-10 23:31:14 +0100

    added piramide
    i may be a little stubborn

---
+++ lab 2/piramide/script.js
@@ -0,0 +1,9 @@
+numberList = [] // waarom is dit nodig?
+piramideLayer = "";
+
+size = prompt("enter piramid size");
+
+for (x = 0; x <= size; x ++){
+    piramideLayer += x;
+    console.log(piramideLayer);
+}
